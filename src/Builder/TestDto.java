c52c6f93f6eb0ef7f07ebd702445ba2f325fed3c
dhparekh <dhparekh@paypal.com>
2023-07-04 13:26:52 +0530

    added builder pattern

---
+++ src/Builder/TestDto.java
@@ -0,0 +1,17 @@
+package Builder;
+
+import java.time.LocalDate;
+
+
+public class TestDto {
+
+    public static void main(String[] args) {
+
+        UserDTO.UserDTOBuilder builder = new UserDTO.UserDTOBuilder();
+        UserDTO userDTO =
+                builder.withBirthday(LocalDate.now().minusYears(24)).withFirstName("Dhairya").withLastName("Parekh").build();
+        System.out.println(userDTO.toString());
+    }
+
+
+}
