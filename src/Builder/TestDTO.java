a5730aac5a01a41a5ba1b1dbb01e00ca11d6ed2b
dhparekh <dhparekh@paypal.com>
2023-07-06 19:27:07 +0530

    prototype addition

--- src/Builder/TestDTO.java
+++ src/Builder/TestDTO.java
@@ -1,10 +1,9 @@
 package Builder;
 
 import java.time.LocalDate;
-import java.util.Calendar;
 
 
-public class TestDto {
+public class TestDTO {
 
     public static void main(String[] args) {
 
---
+++ src/Prototype/General.java
@@ -0,0 +1,29 @@
+package Prototype;
+
+//Doesn't support cloning
+public class General extends GameUnit{
+
+	private String state = "idle";
+	
+	public void boostMorale() {
+		this.state = "MoralBoost";
+	}
+
+	@Override public String toString() {
+		final StringBuilder sb = new StringBuilder("General{");
+		sb.append("state='").append(state).append('\'');
+		sb.append('}');
+		return sb.toString();
+	}
+
+	@Override
+	public GameUnit clone() throws CloneNotSupportedException {
+		throw new CloneNotSupportedException("Ganerals are unique");
+	}
+
+	@Override
+	protected void reset() {
+		throw new UnsupportedOperationException("Reset not supported");
+	}
+	
+}
